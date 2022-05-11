74477171e49128abcddfe24a7632ea69c4cc062c
Giovana Raphaelli <giovanaraphaelli@outlook.com>
2022-05-11 15:24:23 -0300

    refatoracao

--- imc.js
+++ imc.js
@@ -1,33 +1,48 @@
-const calcular = document.getElementById("calcular");
+function calculaIMC() {
+  const calcular = document.getElementById("calcular");
 
-function imc() {
-  const nome = document.getElementById("nome").value;
-  const altura = document.getElementById("altura").value;
-  const peso = document.getElementById("peso").value;
-  const resultado = document.getElementById("resultado");
+  function imc() {
+    const nome = document.getElementById("nome").value;
+    const altura = document.getElementById("altura").value;
+    const peso = document.getElementById("peso").value;
+    const resultado = document.getElementById("resultado");
 
-  if (nome !== "" && altura != "" && peso !== "") {
-    const valorIMC = (peso / (altura * altura)).toFixed(1);
-    let classificacao = "";
+    if (nome !== "" && altura != "" && peso !== "") {
+      const valorIMC = (peso / (altura * altura)).toFixed(1);
+      let classificacao = "";
 
-    if (valorIMC < 18.5) {
-      classificacao = "abaixo do peso.";
-    } else if (valorIMC < 25) {
-      classificacao = "com peso ideal. Parabéns!";
-    } else if (valorIMC < 30) {
-      classificacao = "levemente acima do peso.";
-    } else if (valorIMC < 35) {
-      classificacao = "com Obesidade Grau I.";
-    } else if (valorIMC < 40) {
-      classificacao = "com Obesidade Grau II.";
+      if (valorIMC < 18.5) {
+        classificacao = "abaixo do peso.";
+      } else if (valorIMC < 25) {
+        classificacao = "com peso ideal. Parabéns!";
+      } else if (valorIMC < 30) {
+        classificacao = "levemente acima do peso.";
+      } else if (valorIMC < 35) {
+        classificacao = "com Obesidade Grau I.";
+      } else if (valorIMC < 40) {
+        classificacao = "com Obesidade Grau II.";
+      } else {
+        classificacao = "com Obesidade Grau III. Cuidado!";
+      }
+
+      resultado.textContent = `${nome}, seu IMC é ${valorIMC} e você está ${classificacao}`;
     } else {
-      classificacao = "com Obesidade Grau III. Cuidado!";
+      resultado.textContent = "Preencha todos os campos!";
     }
-
-    resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
-  } else {
-    resultado.textContent = "Preencha todos os campos!";
   }
+
+  calcular.addEventListener("click", imc);
 }
 
-calcular.addEventListener("click", imc);
+calculaIMC();
+
+function acionaBtn() {
+  document.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      let btn = document.querySelector("#calcular");
+      btn.click();
+    }
+  });
+}
+
+acionaBtn();
